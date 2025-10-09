ad046b7d12345fbff7b63f3b1cb8adce20458549
Maksim_Semizhonov <m.a.semizhonov@gmail.com>
2025-10-10 02:28:40 +0700

    feat: add implementation of modal-window

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -57,3 +57,30 @@ buttonScroll.addEventListener('click', function () {
         behavior: 'smooth'
     })
 })
+
+// Modal-window
+const priceBtn = document.querySelectorAll('.price__btn');
+const modalWindow = document.querySelector('.modal-window');
+const closeModalBtn = document.querySelector('.modal-window__close');
+
+function openModal() {
+    modalWindow.showModal();
+    document.body.classList.add('no-scroll');
+}
+
+function closeModal() {
+    modalWindow.close();
+    document.body.classList.remove('no-scroll');
+}
+
+priceBtn.forEach(button => {
+    button.addEventListener('click', openModal);
+});
+
+closeModalBtn.addEventListener('click', closeModal);
+
+modalWindow.addEventListener('click', (e) => {
+    if (e.target === modalWindow) {
+        closeModal();
+    }
+});
